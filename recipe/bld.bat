1e9c368bf6b41a7b4eacc4af39227a1d51fbc534
Tim Williams <36856761+tjwilli58@users.noreply.github.com>
2025-11-06 08:51:11 -0500

    update from 1.26.3 to 1.26.6 (#75)
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,6 +4,11 @@
 :: (set as env var so it's used by both meson and during build with g-ir-scanner)
 set "PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig;%BUILD_PREFIX%\Library\lib\pkgconfig"
 
+copy %LIBRARY_LIB%\libmp3lame.lib %LIBRARY_LIB%\mp3lame.lib
+copy %LIBRARY_LIB%\libx265.lib %LIBRARY_LIB%\x265.lib
+copy %LIBRARY_LIB%\libbz2.lib %LIBRARY_LIB%\bz2.lib
+copy %LIBRARY_LIB%\libxml2.lib %LIBRARY_LIB%\xml2.lib
+
 :: get mixed path (forward slash) form of prefix so host prefix replacement works
 set "LIBRARY_PREFIX_M=%LIBRARY_PREFIX:\=/%"
 
@@ -17,3 +22,7 @@ ninja -C builddir install -j %CPU_COUNT%
 if errorlevel 1 exit 1
 
 del %LIBRARY_PREFIX%\bin\*.pdb
+del %LIBRARY_LIB%\mp3lame.lib
+del %LIBRARY_LIB%\x265.lib
+del %LIBRARY_LIB%\bz2.lib
+del %LIBRARY_LIB%\xml2.lib
